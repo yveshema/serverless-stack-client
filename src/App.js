15a48d42eb48480e88a268753cc6aa796a48d467
Yves Shema <admin@rysapp.com>
2020-04-12 23:42:07 -0700

    Implement user functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
 import Routes from './Routes';
+import { Auth } from 'aws-amplify';
+
+import { AppContext } from './libs/contextLib';
 
 import './App.css';
 
 function App() {
-  return (
+  const [isAuthenticating, setIsAuthenticating] = useState(true);
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+  const history = useHistory();
+
+  useEffect(() => {
+    onLoad();
+  }, []);
+
+  async function onLoad() {
+    try {
+      await Auth.currentSession();
+      userHasAuthenticated(true);
+    }
+    catch (e) {
+      if (e !== 'No current user') {
+        alert(e);
+      }
+    }
+    setIsAuthenticating(false);
+  }
+
+  async function handleLogout(){
+    await Auth.signOut();
+    userHasAuthenticated(false);
+    history.push('/login');
+  }
+
+  return(
+    !isAuthenticating &&
     <div className="App container">
       <Navbar fuild collapseOnSelect>
         <Navbar.Header>
@@ -19,16 +50,23 @@ function App() {
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav pullRight>
-            <LinkContainer to="/signup">
-              <NavItem>Signup</NavItem>
-            </LinkContainer>
-            <LinkContainer to="/login">
-              <NavItem>Login</NavItem>
-            </LinkContainer>
+            { isAuthenticated
+              ? <NavItem onClick={handleLogout}>Logout</NavItem>
+              : <>
+                  <LinkContainer to="/signup">
+                    <NavItem>Signup</NavItem>
+                  </LinkContainer>
+                  <LinkContainer to="/login">
+                    <NavItem>Login</NavItem>
+                  </LinkContainer>
+                </>
+            }            
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-      <Routes />
+      <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
+        <Routes />
+      </AppContext.Provider>
     </div>
   );
 }
--- src/Routes.js
+++ src/Routes.js
@@ -2,12 +2,24 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './containers/Home';
 import NotFound from './containers/NotFound';
+import Login from './containers/Login';
+import Signup from './containers/Signup';
+import NewNote from './containers/NewNote';
 
 export default function Routes(){
     return (
         <Switch>
             <Route exact path="/">
                 <Home />
+            </Route>
+            <Route exact path="/login">
+                <Login />
+            </Route>
+            <Route exact path="/signup">
+                <Signup />
+            </Route>
+            <Route exact path="/notes/new">
+                <NewNote />
             </Route>
             {/* Finally, catch all unmatched routes */}
             <Route>
