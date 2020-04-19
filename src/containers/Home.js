f01298780ae1ee562697c53dc9443452e675e68c
Yves Shema <admin@rysapp.com>
2020-04-18 20:22:48 -0700

    Update the lander

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import { PageHeader, ListGroup, ListGroupItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import { Link } from 'react-router-dom';
 import { useAppContext } from '../libs/contextLib';
 import { onError } from '../libs/errorLib';
 import './Home.css';
@@ -58,6 +59,14 @@ export default function Home(){
             <div className="lander">
                 <h1>Stratch</h1>
                 <p>A simple note taking app</p>
+                <div>
+                    <Link to="/login" className="btn btn-info btn-lg">
+                        Login
+                    </Link>
+                    <Link to="/signup" className="btn btn-success btn-lg">
+                        Signup
+                    </Link>
+                </div>
             </div>
         );
     }
